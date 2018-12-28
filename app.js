e8aba7f03e54bb81bb890dfeef489fd5a0e8ef6e
abrar000 <abrar@uconnect.pk>
2018-12-28 12:52:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+// local variable declare within the function
+
+function helloWorld (){
+  let abrar = "hiiiiii";
+ document.write(" Local variable Output is: "+abrar);
+  
+}
+helloWorld();
+
+// global variable which we declare variable outside the function
+
+let saba = "hello"
+function greet(){
+  document.write("<br> Global variable Output is: "+saba);
+  
+}
+greet();
